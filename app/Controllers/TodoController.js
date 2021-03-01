38ccdb8c32df493178a65727ff1cf3f96a969f41
Ashley Herrera <ashleyherrera131@gmail.com>
2021-03-01 01:33:44 -0700

    taskrow

--- app/Controllers/TodoController.js
+++ app/Controllers/TodoController.js
@@ -3,9 +3,30 @@ import { todoService } from "../Services/TodoService";
 
 
 function _draw(){
-    
+    let task = ProxyState.tasks
+    let template = ""
+    task.forEach(t=> template += t.Template)
+    // console.log(template)
+    document.getElementById('').innerHTML = template
 }
 
 export default class TodoController{
 
+    constructor(){
+        _draw()
+        ProxyState.on("taskRow", _draw)
+    }
+
+    createTask(event){
+        event.preventDefault();
+        console.log('creating task')
+        let form = event.target
+        console.log(form)
+        
+        let rawTask = {
+          title: form.title.value,
+        }
+        console.log(rawTask)
+        todoService.createTask(rawTask)
+      }
 }
--- app/main.js
+++ app/main.js
@@ -1,12 +1,13 @@
 import ImageController from "./Controllers/ImageController.js";
 import QuoteController from "./Controllers/QuoteController.js";
+import TodoController from "./Controllers/TodoController.js";
 import WeatherController from "./Controllers/WeatherController.js";
 
 class App {
   imageController = new ImageController();
   quoteController = new QuoteController();
   weatherController = new WeatherController();
-  
+  todoController = new TodoController();
 }
 
 window["app"] = new App();
